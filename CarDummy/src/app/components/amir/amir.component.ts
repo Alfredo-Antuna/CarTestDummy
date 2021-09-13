4d33814c1ddf444a8b7b9cdf3c0ff2180252b8ce
LTIUser <LTIUser@INFEDL31176.lntinfotech.com>
2021-09-13 14:39:10 -0400

    amir component was created

---
+++ CarDummy/src/app/components/amir/amir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amir',
+  templateUrl: './amir.component.html',
+  styleUrls: ['./amir.component.css']
+})
+export class AmirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
